3c9bb07bf6905480504df393032c0ff720a7c63d
Unfinishedgod <38911560+Unfinishedgod@users.noreply.github.com>
2019-12-19 13:45:47 +0900

    Create list_file_function.R

---
+++ list_file_function.R
@@ -0,0 +1,14 @@
+# list.files(path=".",             # 어떤 디렉토리의 파일 목록을 받아올지 지정
+#            
+#            pattern = NULL,    # 파일 목록중 해당 패턴에 맞는 파일만 불러옴
+#            
+#            all.files = F,        # 숨겨진 파일도 포함할지 여부
+#            
+#            full.names = F,    # 파일 디렉토리도 포함할지 여부
+#            
+#            recursive = F,      # 하위 디렉토리에서도 검색할지 여부
+#            
+#            ignore.case = F,   # 
+#            
+#            include.dirs = F    # 하위 디렉토리의 이름도 포함여부?
+# )
