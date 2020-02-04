60bb5cf7144be3bca893df2d88520f363ec19783
Unfinishedgod <shjj08@gmail.com>
2020-02-04 13:56:41 +0900

    rjava KoNLP install

---
+++ rJava_KoNLP_install.R
@@ -0,0 +1,10 @@
+
+
+
+# $R CMD javareconf
+
+install.packages("rJava")
+library(rJava)
+
+install.packages(c('rJava', 'hash', 'tau', 'Sejong', 'RSQLite'))
+install.packages("https://cran.r-project.org/src/contrib/Archive/KoNLP/KoNLP_0.80.2.tar.gz", repos=NULL, type="source")
