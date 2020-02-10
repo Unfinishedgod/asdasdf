e513f8e1c0140a5d6a4edfe72d84998a33e225d7
root <root@ip-172-31-47-130.ap-northeast-2.compute.internal>
2020-02-10 12:32:59 +0000

    tidy text exam

---
+++ tidytext_test.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(tidytext)
+
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+
+
+text_df <- data_frame(line=1:4, text=text)
+
+text_df
+
+text_df %>%
+  unnest_tokens(word, text)
