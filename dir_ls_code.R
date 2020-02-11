30f6224b3e0b933660a8f33e623859b42aef691c
root <root@ip-172-31-47-130.ap-northeast-2.compute.internal>
2020-02-11 08:21:52 +0000

    dir_ls code

---
+++ dir_ls_code.R
@@ -0,0 +1,17 @@
+# 경로 확인 코드 등등
+dir_ls
+
+dir_info(getwd())
+
+dir_ls(getwd())
+
+dir_ls(getwd(), type = "dir")
+
+dir_ls(getwd(), glob = "*.Rmd")
+
+
+
+
+
+
+
---
+++ set_rJava_Konlp.R
@@ -0,0 +1,16 @@
+# R RMD javareconf
+
+# r 종료 후 다시 키기
+
+# install.packages("rJava")
+library(rJava)
+
+
+# 의존성 패키지 설치
+install.packages(c('stringr', 'hash', 'tau', 'Sejong', 'RSQLite', 'devtools'), type = "binary")
+
+# github 버전 설치
+install.packages("remotes")
+remotes::install_github('haven-jeon/KoNLP')
+
+library(KoNLP)
