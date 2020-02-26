b7626aa26507a82e0b6bfa3a3b7c21047248b2bb
Unfinishedgod <shjj08@gmail.com>
2020-02-26 01:43:23 +0000

    asdf

---
+++ test_apr.R
@@ -0,0 +1,65 @@
+library(wordcloud)
+library(wordcloud2)
+library(KoNLP)
+library(rvest)
+library(tidyverse)
+library(glue)
+
+news_url <-"https://search.naver.com/search.naver?&where=news&query=%EB%8D%B0%EC%9D%B4%ED%84%B0%203%EB%B2%95&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so:r,p:all,a:all&mynews=0&cluster_rank=65&start=1&refresh_start=0"
+
+
+news_html <- read_html(news_url) 
+
+news_urls <- news_html %>% 
+  html_nodes(".news.mynews.section._prs_nws") %>% 
+  html_nodes("._sp_each_url") %>% 
+  html_attr("href")
+
+news_urls <- news_urls[news_urls %>% str_detect("news.naver")]
+
+news_text <- map_chr(news_urls, function(news_page){
+  news_page %>% 
+    read_html() %>% 
+    html_nodes("#articleBodyContents._article_body_contents") %>% 
+    html_text()
+})
+
+cleanging_text <- news_text %>% 
+  str_remove_all("flash 오류를 우회하기 위한 함수 추가") %>% 
+  str_remove_all("function _flash_removeCallback") %>% 
+  str_remove_all("[a-zA-Z]") %>% 
+  str_replace_all("\\W"," ")
+
+
+# cleanging_text <- paste0(cleanging_text, collapse = "")
+
+nouns <- KoNLP::extractNoun(cleanging_text)
+
+# table 형태로 변환
+wordcount <- table(unlist(nouns))
+
+df.word  <- as.data.frame(wordcount, stringsAsFactors = FALSE)
+df.word <- rename(df.word, word = Var1, freq = Freq)
+
+word.freq  <- df.word %>% 
+  filter(nchar(word) >=2 & freq >= 2) %>% 
+  arrange(desc(freq)) 
+
+
+wordcloud2(word.freq,
+           fontFamily="Malgun Gothic",
+           size = 0.5,
+           minRotation=0,
+           maxRotation=0,
+)
+
+
+ggplot(head(word.freq,30), aes(x=reorder(word,freq),y=freq,fill=word),colour=gradient) +
+  geom_col() +
+  theme_bw() + 
+  labs(x="",y="단어 빈도",title = "데이터 3법 네이버 뉴스 단어 빈도") + 
+  coord_flip() + 
+  theme(legend.position = "none") +
+  theme(text = element_text(size=20),
+        axis.text.x = element_text(angle=90, hjust=1)) 
+
--- tidytext_test.R
+++ tidytext_test.R
@@ -1,15 +1,64 @@
 library(tidyverse)
 library(tidytext)
+library(arules)
+library(KoNLP)
 
 text <- c("Because I could not stop for Death -",
           "He kindly stopped for me -",
           "The Carriage held but just Ourselves -",
           "and Immortality")
 
+paste0(text, collapse = ",")
 
-text_df <- data_frame(line=1:4, text=text)
+text <- cleanging_text
 
-text_df
+text_df <- data_frame(line=1:length(text), text=text)
 
-text_df %>%
+asdf <- text_df %>%
   unnest_tokens(word, text)
+
+asdf_2 <- map(text, function(x) {
+  text_df <- data_frame(line=1:length(x), text=x)
+  
+  text_df %>%
+    unnest_tokens(word, text) %>% 
+    top_n(10) %>% 
+    select(word) %>% 
+    as.character()
+})
+
+asdf
+
+asdf %>% 
+  group_by(line) %>% 
+  summarise(asdfasdf = paste0(word, seq = FALSE))
+
+
+a <- text_df %>%
+  unnest_tokens(word, text) %>% 
+  count(word, sort= TRUE)
+
+
+a
+
+
+
+buyItems <- as(asdf_2, "transactions")
+
+# 변환된 트랜잭션 확인(11개 항목에 대해 5개 거래 존재)
+buyItems
+## transactions in sparse format with
+##  5 transactions (rows) and
+##  11 items (columns)
+# 트랜잭션 데이터는 inspect 함수를 통해 내용 확인
+inspect(buyItems)
+
+
+buyItemResult <- apriori(buyItems, parameter = list(support=0.1, confidence=0.3))
+
+
+# 도출된 연관성 규칙 5개만 확인
+buyItemResult[1:5]
+## set of 5 rules
+# 연관성 규칙 상세 보기
+inspect(buyItemResult[1:5])
