309092b93def5764cd6c3723c5ea4dfe7ae0a68c
Unfinishedgod <shjj08@gmail.com>
2020-04-26 22:10:56 +0900

    h2o

---
+++ h20_install.R
@@ -0,0 +1,4 @@
+library(h2o)
+update.packages("h2o")
+h2o.init(nthreads = -1, #Number of threads -1 means use all cores on your machine
+         max_mem_size = "8G")  #max mem size is the maximum memory to allocate to H2O
